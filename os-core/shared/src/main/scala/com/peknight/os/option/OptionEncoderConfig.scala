e1066e45425ec110c5268fdc3f72d4ef6632cf7c
peknight <JKpeknight@gmail.com>
2025-08-15 11:24:28 +0800

    update

--- os-core/shared/src/main/scala/com/peknight/os/option/OptionEncoderConfig.scala
+++
@@ -1,7 +0,0 @@
-package com.peknight.os.option
-
-trait OptionEncoderConfig:
-  def transformMemberOptionKey: String => OptionKey
-  def booleanAsFlag: Boolean
-  def transformConstructorOption: String => Option[(OptionKey, List[String])]
-end OptionEncoderConfig
--- os-core/shared/src/main/scala/com/peknight/os/option/OptionType.scala
+++
@@ -1,8 +0,0 @@
-package com.peknight.os.option
-
-enum OptionType(val prefix: String):
-  case LongOption extends OptionType("--")
-  case ShortOption extends OptionType("-")
-  case NonStandardOption extends OptionType("-")
-  case BSDOption extends OptionType("")
-end OptionType
